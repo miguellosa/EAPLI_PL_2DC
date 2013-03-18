bb13ad431fb270f3c1bcd84f6bbe88e8fa1ff95b
miguellosa <jal@isep.ipp.pt>
2013-03-18 18:54:23 +0000

    alterações ao projecto base

---
+++ eapli.ExpenseManager/src/Controllers/ExpenseRegisterController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Model.Expense;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import Persistence.*;
+/**
+ *
+ * @author Paulo Gandra Sousa
+ */
+public class ExpenseRegisterController {
+
+    public ExpenseRegisterController() {
+    }
+
+    public void registerExpense(String what, Date date, BigDecimal amount) {
+        Expense expense = new Expense( what, date, amount);
+        IExpenseRepository repo = new ExpenseRepository();
+        repo.save(expense);
+    }   
+}
---
+++ eapli.ExpenseManager/src/Presentation/ExpenseManager.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+import Presentation.MainMenu;
+import eapli.util.Math;
+import Persistence.*;
+import eapli.bootstrap.ExpenseBootstrapper;
+
+/**
+ *
+ * @author Paulo Gandra Sousa
+ */
+public class ExpenseManager {
+        
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ExpenseBootstrapper e=new ExpenseBootstrapper();
+        
+        MainMenu menu = new MainMenu();
+        menu.mainLoop();
+   
+    }
+}
