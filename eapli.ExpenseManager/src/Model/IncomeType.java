34bd92f2ad12f0c5c03fa39ca27da91cb895b03e
i090566 <i090566@PCI091.dei.isep.ipp.pt>
2013-03-20 14:17:13 +0000

    Tipos de entrada de rendimento e repositório.

---
+++ eapli.ExpenseManager/src/Model/IncomeType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author i090566
+ */
+public class IncomeType {
+    private String description;
+	
+    public IncomeType(String d) {
+            description = d;
+    }
+}
+
