011de047801a0a8cd51cfa88b9dfb701dcb281b0
1110620MiguelPereira <miguelgopereira@gmail.com>
2013-04-03 14:32:10 +0100

    Created inheritance from BaseUI

--- eapli.ExpenseManager/src/Presentation/UnavailableFunctionUI.java
+++ eapli.ExpenseManager/src/Presentation/UnavailableFunctionUI.java
@@ -4,7 +4,22 @@ package Presentation;
  * User interface to be called when functions aren't implemented yet
  * @author João Carreira
  */
-public class UnavailableFunctionUI {
+public class UnavailableFunctionUI extends BaseUI {
+
+    @Override
+    public void show() {
+        headline();
+        mainLoop();
+    }
+    
+    @Override
+    protected void headline() {
+        System.out.println("======================");
+        System.out.println("  Configurations  ");
+        System.out.println("======================\n");
+    }
+
+    @Override
     public void mainLoop(){
         System.out.println("Unavailable function!\nPlease try again!\n\n");
     }
