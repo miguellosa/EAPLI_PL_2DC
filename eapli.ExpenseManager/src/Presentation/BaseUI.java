9b75d5e58e1424d01853ea844e97008aba6c5394
1110620MiguelPereira <miguelgopereira@gmail.com>
2013-04-03 14:22:33 +0100

    Created abtract class BaseUI

---
+++ eapli.ExpenseManager/src/Presentation/BaseUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+/**
+ *
+ * @author Miguel
+ */
+public abstract class BaseUI {
+    
+    public abstract void show();
+    
+    protected abstract void headline();
+    
+    protected abstract void mainLoop();
+    
+    //TO DO definir controlador abstracto
+    
+}
