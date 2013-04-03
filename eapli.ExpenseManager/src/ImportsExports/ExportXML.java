0e7a11ace92e1225eeb53d014a49114931a95bc2
i100447 <i100447@PCI087.dei.isep.ipp.pt>
2013-04-03 14:52:16 +0100

    Criação do package ImportsExports e classes e importaçao e exportação XML

---
+++ eapli.ExpenseManager/src/ImportsExports/ExportXML.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ImportsExports;
+
+/**
+ *
+ * @author i100447
+ */
+public class ExportXML {
+    
+}
